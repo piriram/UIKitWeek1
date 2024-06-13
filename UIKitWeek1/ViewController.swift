5eb33f27664f29d96ee0f785ffddae1c10b53ae5
piri <pyoram25@gmail.com>
2024-06-13 21:18:40 +0900

    Feat: 코드 추가

--- UIKitWeek1/ViewController.swift
+++ UIKitWeek1/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TitleLabel: UILabel!
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    let correctEmail = "pyoram25@naver.com"
+    let correctPassword = "judyjoha"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        PasswordTextField.isSecureTextEntry = true
     }
 
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        let enteredEmail = EmailTextField.text
+                let enteredPassword = PasswordTextField.text
 
+                // 입력된 이메일과 패스워드가 정해진 값과 동일한지 확인합니다.
+                if enteredEmail == correctEmail && enteredPassword == correctPassword {
+                    // 동일하다면 TitleLabel의 텍스트를 "complete"로 변경합니다.
+                    TitleLabel.text = "로그인 성공"
+                } else {
+                    // 동일하지 않다면 오류 메시지를 표시할 수도 있습니다.
+                    TitleLabel.text = "로그인 실패"
+                }
+    }
+    
 }
 
